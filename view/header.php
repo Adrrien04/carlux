98096b78b70c9767ab6005493eddcb4f76b96500
Adrrien04 <adrrien.chandrakumar@gmail.com>
2023-12-20 11:37:46 +0100

    modification header

--- view/header.php
+++ view/header.php
@@ -6,6 +6,7 @@ $listeVoitures = $voitureModel->trierVoitures($critereTri);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
     <style>
@@ -14,6 +15,7 @@ $listeVoitures = $voitureModel->trierVoitures($critereTri);
         }
     </style>
 </head>
+
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -28,10 +30,11 @@ $listeVoitures = $voitureModel->trierVoitures($critereTri);
             <li class="nav-item">
                 <a class="nav-link" href="../index.php">Accueil</a>
             </li>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ajouterVoitureModal">
-                Ajouter un véhicule
-            </button>
-
+            <li class="nav-item">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ajouterVoitureModal">
+                    Ajouter un véhicule
+                </button>
+            </li>
             <li class="nav-item">
                 <form class="form-inline" action="../main.php" method="get">
                     <label for="tri" class="mr-2">Trier par :</label>
@@ -43,15 +46,16 @@ $listeVoitures = $voitureModel->trierVoitures($critereTri);
                         <option value="annee_desc" <?php echo ($critereTri === 'annee_desc') ? 'selected' : ''; ?>>Année décroissant</option>
                         <option value="kilometrage_asc" <?php echo ($critereTri === 'kilometrage_asc') ? 'selected' : ''; ?>>Kilométrage croissant</option>
                         <option value="kilometrage_desc" <?php echo ($critereTri === 'kilometrage_desc') ? 'selected' : ''; ?>>Kilométrage décroissant</option>
-
                     </select>
                 </form>
-            </li class="nav-item">
-            <form class="form-inline" action="/view/rechercher_voiture.php" method="get">
-                <label for="searchQuery">Recherche :</label>
-                <input type="text" id="searchQuery" name="q" placeholder="Marque, Modèle, Couleur, etc.">
-                <button type="submit" class="btn btn-primary">Rechercher</button>
-            </form>
+            </li>
+            <li class="nav-item">
+                <form class="form-inline" action="/view/rechercher_voiture.php" method="get">
+                    <label for="searchQuery">Recherche :</label>
+                    <input type="text" id="searchQuery" name="q" placeholder="Marque, Modèle, Couleur, etc.">
+                    <button type="submit" class="btn btn-primary">Rechercher</button>
+                </form>
+            </li>
         </ul>
     </div>
 </nav>
@@ -60,3 +64,6 @@ $listeVoitures = $voitureModel->trierVoitures($critereTri);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+</body>
+
+</html>
